c9466ca92c28a0d656c2d679d59da0ac1610d870
王静雨 <982887078@qq.com>
2017-04-13 21:53:23 +0800

    添加获取属性列表的方法，并返回数组

--- 运行时 关联对象/运行时 关联对象/NSObject+Runtime.m
+++ 运行时 关联对象/运行时 关联对象/NSObject+Runtime.m
@@ -7,10 +7,37 @@
 //
 
 #import "NSObject+Runtime.h"
+#import <objc/runtime.h>
 
 @implementation NSObject (Runtime)
 
+
 +(NSArray *)wjy_objcProtyList {
+    
+    //获得属性列表数组
+    //c语言中，数组的名字就是指向第一个元素的地址
+    unsigned int count = 0;
+    objc_property_t *proty = class_copyPropertyList([self class], &count);
+    
+    
+    NSMutableArray *array = [NSMutableArray array];
+    //遍历数组
+    for (unsigned int i = 0;  i < count; i++) {
+        
+        //获取属性的名称
+        
+        objc_property_t pty = proty[i];
+        
+        const char *Name = property_getName(pty);
+        
+        NSString *str = [NSString stringWithCString:Name encoding:NSUTF8StringEncoding];
+        
+        [array addObject:str];
+    }
+    
+    free(proty);
+    
+    return array.copy;
     
 }
 
